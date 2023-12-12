3060d1d84f7c7f3fba2acd1bb4e433387c504353
Tai-Hsien (Bruce) Wu <Tai-Hsien@users.noreply.github.com>
2023-12-12 17:47:19 -0500

    updated meshsegnet_feature_process
    removed output X in previous version.
    The only output mesh contains X as "Input_Features"

--- easy_mesh.py
+++ easy_mesh.py
@@ -9,13 +9,14 @@ import time
 # it is designed to be easy to use and easy to understand
 # current vedo version: 2023.4.6
 
-def meshsegnet_feature_process(target_mesh, check_manifold=False, need_decimate=False, target_ncells=10000, feature_options=['cells', 'barycenters', 'cell_normals']):
+def meshsegnet_feature_process(target_mesh, check_manifold=False, need_decimate=False, decimate_basis='cell', target_numbers=10000, feature_options=['cells', 'barycenters', 'cell_normals']):
     '''
     input:
         target_mesh: a vedo mesh object
         check_manifold: boolean; whether to check if the mesh is manifold
         need_decimate: boolean; whether to decimate the mesh to target_ncells
-        target_ncells: int; target number of cells after decimation
+        decimate_basis: 'cell' or 'point'; decimate based on cells or points
+        target_numbers: int; target number of cells or points after decimation
         feature_options: ['cells', 'barycenters', 'cell_normals'] for MeshSegNet; either 'points' or 'cells' musts be the first option
                          current options: 'points' (NPx3), 'cells' (NCx9), 'barycenters' (NCx3), 'cell_normals' (NCx3), 'cell_curvatures' (NCx1), 'cell_densities (NCx3)'
     output: a numpy array for the input features of segmentation model (e.g., MeshSegNet) in either training or inference phases based on the feature_scheme
@@ -33,10 +34,12 @@ def meshsegnet_feature_process(target_mesh, check_manifold=False, need_decimate=
             else:
                 print('Repair failed')
         
-    if need_decimate == True: 
-        target_num = target_ncells
-        ratio = target_num/mesh.ncells # calculate ratio
-        mesh = mesh.decimate(fraction=ratio)
+    if need_decimate == True:
+        if decimate_basis == 'cell':
+            ratio = target_numbers/mesh.ncells # calculate ratio
+            mesh = mesh.decimate(fraction=ratio)
+        elif decimate_basis == 'point':
+            mesh = mesh.decimate(n=target_numbers)
 
     # move points into origin
     # mesh.points(mesh.points() - mean_cell_centers[0:3])
@@ -154,6 +157,8 @@ def meshsegnet_feature_process(target_mesh, check_manifold=False, need_decimate=
                 X = np.column_stack((X, point_densities))
                 mesh.pointdata['Normalized_Densities'] = point_densities
 
+        mesh.pointdata['Input_Features'] = X
+
     elif 'cells' in feature_options:
         X = cells
         for i_feature in feature_options:
@@ -172,7 +177,9 @@ def meshsegnet_feature_process(target_mesh, check_manifold=False, need_decimate=
                 X = np.column_stack((X, cell_densities))
                 mesh.celldata['Normalized_Densities'] = cell_densities
 
-    return X, mesh
+        mesh.celldata['Input_Features'] = X
+
+    return mesh
     
 
 def mesh_grah_cut_optimization(target_mesh, label_probability, lambda_c=30, label_name='Label'):
@@ -204,9 +211,9 @@ def mesh_grah_cut_optimization(target_mesh, label_probability, lambda_c=30, labe
     pairwise = 1 - np.eye(num_classes, dtype=np.int32)
 
     #edges
-    try:
+    if 'Normals' in target_mesh.celldata.keys():
         normals = target_mesh.celldata['Normals']
-    except:
+    else:
         target_mesh.compute_normals()
         normals = target_mesh.celldata['Normals']
         
@@ -229,7 +236,7 @@ def mesh_grah_cut_optimization(target_mesh, label_probability, lambda_c=30, labe
         connected_cells_ids = np.intersect1d(p1_connected_cells_ids, p2_connected_cells_ids)
         if len(connected_cells_ids) == 2:
             shared_edges.append(connected_cells_ids)
-    shared_edges = np.array(shared_edges) # share_edge: [cell1_id, cell2_id]
+    shared_edges = np.array(shared_edges) # shared_edge: [cell1_id, cell2_id]
 
     cos_theta = np.dot(normals[shared_edges[:, 0], 0:3], normals[shared_edges[:, 1], 0:3].transpose()).diagonal()/np.linalg.norm(normals[shared_edges[:, 0], 0:3], axis=1)/np.linalg.norm(normals[shared_edges[:, 1], 0:3], axis=1)
     cos_theta[cos_theta >= 1.0] = 0.9999
@@ -250,6 +257,80 @@ def mesh_grah_cut_optimization(target_mesh, label_probability, lambda_c=30, labe
 
     new_mesh = target_mesh.clone()
     new_mesh.celldata['Label'] = refine_labels
+    end_time = time.time()
+    total_time = end_time - start_time
+    print("---Label has been refined: {} seconds ---".format(total_time))
+
+    return new_mesh
+
+
+def point_grah_cut_optimization(target_mesh, label_probability, lambda_c=3, label_name='Label'):
+    '''
+    required packages: pygco; this function has been optimized by vecteroization
+    inputs:
+        target_mesh: a vedo mesh object
+        label_probability: a numpy array of shape (num_cells, num_classes)
+        num_classes: int; number of classes
+        lambda_c: float; parameter for the edge weight
+        label_name: string; name of the label array in the target_mesh
+    Note: The optimal lambda_c is very different (different scales) to mesh-based graph-cut
+    '''
+    from pygco import cut_from_graph
+
+    start_time = time.time()
+
+    label_probability = label_probability.squeeze()
+    print(label_probability)
+    print(label_probability.shape)
+    num_classes = label_probability.shape[1]
+
+    round_factor = 100
+    label_probability[label_probability < 1.0e-6] = 1.0e-6
+
+    # unaries
+    unaries = -round_factor * np.log10(label_probability)
+    unaries = unaries.astype(np.int32)
+    unaries = unaries.reshape(-1, num_classes)
+
+    # parawise
+    pairwise = 1 - np.eye(num_classes, dtype=np.int32)
+
+    #edges
+    if 'Normals' in target_mesh.pointdata.keys():
+        normals = target_mesh.pointdata['Normals']
+    else:
+        target_mesh.compute_normals()
+        normals = target_mesh.pointdata['Normals']
+
+    # points
+    points = target_mesh.points()
+
+    # find all edges [vedo build-in function is wrong]
+    all_cells = np.array(target_mesh.cells())
+    edge1 = np.sort(all_cells[:, [0, 1]])
+    edge2 = np.sort(all_cells[:, [0, 2]])
+    edge3 = np.sort(all_cells[:, [1, 2]])
+    all_unique_edges = np.unique(np.concatenate((edge1, edge2, edge3), axis=0), axis=0)
+
+    cos_theta = np.dot(normals[all_unique_edges[:, 0], 0:3], normals[all_unique_edges[:, 1], 0:3].transpose()).diagonal()/np.linalg.norm(normals[all_unique_edges[:, 0], 0:3], axis=1)/np.linalg.norm(normals[all_unique_edges[:, 1], 0:3], axis=1)
+    cos_theta[cos_theta >= 1.0] = 0.9999
+    theta = np.arccos(cos_theta)
+    phi = np.linalg.norm(points[all_unique_edges[:, 0], :] - points[all_unique_edges[:, 1], :], axis=1)
+    
+    beta = 1 + np.linalg.norm((np.dot(normals[all_unique_edges[:, 0], 0:3], normals[all_unique_edges[:, 1], 0:3].transpose()).diagonal()).reshape(-1, 1), axis=1)
+    edges = -beta*np.log10(theta/np.pi)*phi
+    edges2 = -np.log10(theta/np.pi)*phi
+    theta_mask = theta > np.pi/2.0
+    edges[theta_mask] = edges2[theta_mask]
+    edges = np.concatenate([all_unique_edges, edges.reshape(-1, 1)], axis=1)
+    edges[:, 2] *= lambda_c*round_factor
+    edges = edges.astype(np.int32)
+
+    refine_labels = cut_from_graph(edges, unaries, pairwise)
+    refine_labels = refine_labels.reshape([-1, 1])
+
+    new_mesh = target_mesh.clone()
+    new_mesh.pointdata['Label'] = refine_labels
     end_time = time.time()
     total_time = end_time - start_time
     print("---Label has been refined: {} seconds ---".format(total_time))
@@ -321,19 +402,19 @@ def expand_selection(main_mesh, partial_mesh, n_loop=1):
 if __name__ == '__main__':
 
     upper_mesh = vedo.load('Example_01.vtp')
-    upper_mesh_d = vedo.load('Example_02_d.vtp')
+    # upper_mesh_d = vedo.load('Example_02_d.vtp')
     # upper_mesh = vedo.load('Example_03.vtp')
+    # upper_mesh = vedo.load('00OMSZGW_upper_point.vtp')
 
+    #------------------------------------------------------------------------------------
     # meshsegnet example
     # cell-based
-    X, mesh_d = meshsegnet_feature_process(upper_mesh_d, check_manifold=True, need_decimate=True, target_ncells=10000, feature_options=['cells', 'cell_curvatures', 'cell_normals', 'cell_densities', 'barycenters'])
-    print(X.shape, mesh_d.ncells)
+    mesh_d = meshsegnet_feature_process(upper_mesh, check_manifold=True, need_decimate=True, decimate_basis='cell', target_numbers=10000, feature_options=['cells', 'cell_curvatures', 'cell_normals', 'cell_densities', 'barycenters'])
+    print(mesh_d.pointdata.keys(), mesh_d.celldata.keys())
     mesh_d.write(('tmp_Example_01_d_cell_based.vtp'))
     # point-based
-    upper_mesh_d = upper_mesh.decimate(n=10000)
-    print('# of cells and points after decimating', upper_mesh_d.ncells, upper_mesh_d.npoints)
-    X, mesh_d = meshsegnet_feature_process(upper_mesh_d, check_manifold=True, need_decimate=False, target_ncells=10000, feature_options=['points', 'point_normals', 'point_curvatures', 'point_densities'])
-    print(X.shape, mesh_d.npoints)
+    mesh_d = meshsegnet_feature_process(upper_mesh, check_manifold=True, need_decimate=True, decimate_basis='point', target_numbers=10000, feature_options=['points', 'point_normals', 'point_curvatures', 'point_densities'])
+    print(mesh_d.pointdata.keys(), mesh_d.celldata.keys())
     mesh_d.write(('tmp_Example_02_d_point_based.vtp'))
 
     # X, mesh_d = meshsegnet_feature_process(upper_mesh, check_manifold=True, need_decimate=True, target_ncells=10000, feature_options=['cells', 'cell_normals'])
@@ -341,7 +422,9 @@ if __name__ == '__main__':
     # X, mesh_d = meshsegnet_feature_process(upper_mesh, check_manifold=True, need_decimate=True, target_ncells=10000, feature_options=['cells'])
     # print(X.shape)
 
+    #------------------------------------------------------------------------------------
     # graph-cut example
+    # cell-based
     # label_probability = np.zeros([upper_mesh_d.ncells, 17])
     # # init probability; 0.9 for each cell with the current label
     # for i_label in range(17):
@@ -349,7 +432,20 @@ if __name__ == '__main__':
     # refined_label_mesh = mesh_grah_cut_optimization(upper_mesh_d, label_probability, lambda_c = 30, label_name='Label')
     # upper_mesh_d.show().close()
     # refined_label_mesh.show().close()
+
+    # point-based
+    # label_probability = np.zeros([upper_mesh_d.npoints, 17])
+    # # init probability; 0.9 for each cell with the current label
+    # for i_label in range(17):
+    #     label_probability[upper_mesh_d.pointdata['Label']==i_label, i_label] = 0.8
+    # upper_mesh_d.pointdata['Probability'] = label_probability
+    # refined_label_mesh = point_grah_cut_optimization(upper_mesh_d, label_probability, lambda_c=3, label_name='Label')
+    # upper_mesh_d.show().close()
+    # refined_label_mesh.pointdata.select('Label')
+    # refined_label_mesh.show().close()
+    # refined_label_mesh.write('Example_02_d_point_refined.vtp')
     
+    #------------------------------------------------------------------------------------
     # expand_selection example
     # teeth_mesh = upper_mesh.clone().threshold('Label', above=0.5, below=16.5, on='cells').c('red')
     # expanded_teeth_mesh = expand_selection(upper_mesh, teeth_mesh, n_loop=5).c('blue').alpha(0.5)
